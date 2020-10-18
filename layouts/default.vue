ce693d326efbdb1b36a18f67e497eb943419101e
wellsonah2019 <wellsonah.2019@smu.edu.sg>
2020-10-18 17:09:35 +0800

    I've made the navbar inside default.vue and just started working on the app.vue (no API calls made yet currently, i'll start on the API calls for search and the cards ASAP)

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,81 @@
 <template>
   <div>
-    <Nuxt />
+    <div
+      style="
+        padding-top: 10px;
+        position: fixed;
+        background-color: white;
+        width: 100%;
+      "
+    >
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
+      <div>
+        <form>
+          <div class="form-row">
+            <a href="/app">
+              <img
+                src="../logo_2.png"
+                alt="why is this taking so long"
+                style="margin-left: 20px"
+                width="200"
+              />
+            </a>
+            <span style="margin-left: 150px"></span>
+            <div class="form-group has-search" style="margin-top: 10px">
+              <span
+                class="fa fa-search form-control-feedback bg-light"
+                style="margin-top: 6px"
+              ></span>
+              <input
+                type="text"
+                class="form-control bg-light"
+                placeholder="Search"
+                style="
+                  height: 45px;
+                  border: 0px;
+                  width: 750px;
+                  font-size: 18px;
+                  line-height: 40px;
+                "
+                @click="
+                  {
+                    {
+                      created()
+                    }
+                  }
+                "
+              />
+            </div>
+          </div>
+        </form>
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <div id="navbarSupportedContent" class="collapse navbar-collapse">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="#"> Restaurant Directory </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  id="navbarDropdown"
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  Dropdown (if ever needed)
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </div>
+    <nuxt />
   </div>
 </template>
 
@@ -52,4 +127,42 @@ html {
   color: #fff;
   background-color: #35495e;
 }
+
+.has-search .form-control {
+  padding-left: 2.375rem;
+}
+
+.has-search .form-control-feedback {
+  position: absolute;
+  z-index: 2;
+  display: block;
+  width: 2.375rem;
+  height: 2.375rem;
+  line-height: 2.375rem;
+  text-align: center;
+  pointer-events: none;
+  color: #aaa;
+}
 </style>
+
+<script>
+export default {
+  methods: {
+    created() {
+      const request = new XMLHttpRequest()
+      request.onreadystatechange = function () {
+        if (this.status === 200 && this.readyState === 4) {
+          const parsed = JSON.parse(this.responseText)
+          console.log(parsed)
+        }
+      }
+      request.open(
+        'GET',
+        'https://tih-api.stb.gov.sg/content/v1/food-beverages/search?keyword=c&apikey=V9RImjwjM4y4T2GFlnwjdMJuIs3NoHV1',
+        true
+      )
+      request.send()
+    },
+  },
+}
+</script>
---
+++ pages/app.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <br /><br /><br /><br /><br /><br />
+    <div class="header">
+      <br /><br /><br /><br /><br />
+
+      <h3 class="white_text" style="font-size: 50px">
+        Don't know what to eat?
+      </h3>
+      <h5
+        class="white_text"
+        style="margin-left: 42%; font-size: 25px; width: 250px"
+      >
+        discover, book, and dine with us
+      </h5>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    created() {
+      const request = new XMLHttpRequest()
+      request.onreadystatechange = function () {
+        if (this.status === 200 && this.readyState === 4) {
+          const parsed = JSON.parse(this.responseText)
+          console.log(parsed)
+        }
+      }
+      request.open(
+        'GET',
+        'https://tih-api.stb.gov.sg/content/v1/food-beverages/search?keyword=c&apikey=V9RImjwjM4y4T2GFlnwjdMJuIs3NoHV1',
+        true
+      )
+      request.send()
+    },
+  },
+}
+</script>
+
+<style>
+li {
+  margin-left: 20px;
+  margin-right: 20px;
+}
+.header {
+  background-image: url('../header_wallp.jpg');
+  height: 400px;
+  text-align: center;
+}
+.white_text {
+  color: white;
+  font-weight: bold;
+}
+</style>
