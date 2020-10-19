d0f17ded09fdfade038390a25e9a203eee01d1e9
jasxmine <54893485+jasxmine@users.noreply.github.com>
2020-10-19 10:44:18 +0800

    Add dashboard and customer pages

---
+++ pages/dashboard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="header">
+      <br /><br /><br /><br /><br />
+      <br />
+      <br />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  path: '../layout/admin.vue',
+  name: 'Admin',
+  layout: 'admin',
+  meta: { template: 'admin' },
+}
+</script>
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+</style>
