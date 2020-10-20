68079798ddae4178d67ed50d46bda41762af16bc
wellsonah2019 <wellsonah.2019@smu.edu.sg>
2020-10-20 22:57:07 +0800

    added working search on navbar

---
+++ components/search.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div v-for="searchRes in search" :key="searchRes.id">
+      <a href="youtube.com"> {{ searchRes.name }} {{ searchRes.location }} </a>
+      <hr />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search',
+  props: ['search'],
+}
+</script>
+
+<style>
+div {
+  border: 1px black;
+}
+</style>
