f3f0f54b4be1682aaccc19b59b4b0400a08f2f25
Rdice14 <56544853+Rdice14@users.noreply.github.com>
2020-10-24 10:33:29 +0800

    Added dynamically generated checkbox and restaurant list

--- pages/search/index.vue
+++ pages/search/index.vue
@@ -128,14 +128,7 @@ export default {
 
       const pRestaurantDescription = document.createElement('p')
       pRestaurantDescription.classList.add('mb-1')
-      const sampleText = `Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.`
+      const sampleText = `Restaurant Description.`
       pRestaurantDescription.appendChild(document.createTextNode(sampleText))
 
       mainA.appendChild(divTitle)
