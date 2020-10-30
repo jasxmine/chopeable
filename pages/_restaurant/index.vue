26aa70b5e5dc00419cde788b5beb2ce9e27b32ef
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-10-31 02:13:45 +0800

    Add minor stylings for indiv restaurant page

--- pages/_restaurant/index.vue
+++ pages/_restaurant/index.vue
@@ -1,7 +1,21 @@
 <template>
-  <h1>
-    {{ restaurantData.name }}
-  </h1>
+  <div>
+    <div class="container mt-5 p-0">
+      <img :src="restaurantData.featured_image" />
+      <div class="d-flex justify-content-between">
+        <h1>
+          {{ restaurantData.name }}
+        </h1>
+      </div>
+      <h4 class="text-muted">
+        {{ `${restaurantData.establishment} - ${restaurantData.cuisines}` }}
+      </h4>
+      <h5>
+        {{ restaurantData.location && restaurantData.location.locality }}
+      </h5>
+      <p>{{ restaurantData.timings }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -21,7 +35,6 @@ export default {
       .then((res) => {
         return res.restaurants[0].restaurant
       })
-    console.log(this.restaurantData)
   },
 }
 </script>
