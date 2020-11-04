965adfdef7298702024cc9e8b329e850580709c8
Rdice14 <56544853+Rdice14@users.noreply.github.com>
2020-11-04 14:57:21 +0800

    Added toggleable cuisine type checkbox

---
+++ components/search/checkboxComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <form id="cuisineTypes">
+    <div v-for="item in first8" :key="item.id">
+      <input type="checkbox" :value="item.cuisine_name" />
+      <span> {{ item.cuisine.cuisine_name }} </span>
+    </div>
+    <div id="more">
+      <div v-for="item in other" :key="item.id">
+        <input type="checkbox" :value="item.cuisine_name" />
+        <span> {{ item.cuisine.cuisine_name }} </span>
+      </div>
+    </div>
+    <button id="moreButton" type="button" @click="seeMore()">See Mo re</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'CheckboxComponent',
+  props: ['first8', 'other'],
+  methods: {
+    seeMore() {
+      const moreText = document.getElementById('more')
+      const btnText = document.getElementById('moreButton')
+
+      if (moreText.style.display === 'none') {
+        btnText.innerHTML = 'See Less'
+        moreText.style.display = 'block'
+      } else {
+        btnText.innerHTML = 'See More'
+        moreText.style.display = 'none'
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+#more {
+  display: none;
+}
+</style>
--- pages/search/index.vue
+++ pages/search/index.vue
@@ -10,9 +10,13 @@
           <br />
           <br />
           <h4 class="card-subtitle">Cousine Type</h4>
-          <form id="cuisineTypes"></form>
+          <checkboxComponent
+            :key="checkboxId++"
+            :first8="cType.slice(0, 8)"
+            :other="cType.slice(8)"
+          >
+          </checkboxComponent>
           <br />
-          <button type="button" class="btn btn-warning">Search</button>
         </div>
       </div>
       <div id="item" class="col-sm-9">
@@ -53,9 +57,7 @@
                 </p>
               </div>
             </div>
-            <!-- <div class="card-footer">
-              <small class="text-muted">Last updated 3 mins ago</small>
-            </div> -->
+
           </div>
           <cardComponent
             v-for="restaurant in allRestaurants"
@@ -71,20 +73,32 @@
 
 <script>
 import cardComponent from '../../components/search/cardComponent'
+import checkboxComponent from '../../components/search/checkboxComponent'
 import restaurantService from '../../services/restaurantService'
 
 window.axios = require('axios')
 
 export default {
-  components: { cardComponent },
+  components: { cardComponent, checkboxComponent },
   data() {
     return {
       allRestaurants: [],
+      cType: [],
+      checkboxId: 0,
     }
   },
   mounted() {
     // this.getAlpha()
-    this.getCuisineTypes()
+    restaurantService.getCuisines().then((res) => {
+      const cuisineArr = JSON.parse(JSON.stringify(res))
+      for (let i = 0; i < cuisineArr.cuisines.length; i++) {
+        const toAdd = JSON.parse(JSON.stringify(cuisineArr.cuisines[i]))
+        console.log(toAdd)
+        this.cType.push(toAdd)
+        // console.log(cuisineArr.cuisines[i])
+      }
+    })
+    this.observerError(this.cType)
     restaurantService.getRestaurants().then((res) => {
       const restaurantsArr = res.restaurants
       for (let i = 0; i < restaurantsArr.length; i++) {
@@ -93,11 +107,18 @@ export default {
           toAdd.featured_image = '/logo_photo.jpg'
         }
         this.allRestaurants.push(toAdd)
-        console.log(toAdd)
+        // console.log(toAdd)
       }
     })
   },
   methods: {
+    observerError(arr) {
+      arr = JSON.parse(JSON.stringify(arr))
+      for (let i = 0; i < arr.length; i++) {
+        arr[i] = JSON.parse(JSON.stringify(arr[i]))
+        console.log(arr[i])
+      }
+    },
     getAlpha() {
       const alphaList = '#ABCDEFGHIJKLMNOPQRSTUVWXYZ'
       for (let i = 0; i < alphaList.length; i++) {
@@ -115,146 +136,6 @@ export default {
         a.appendChild(strong)
         li.appendChild(a)
         document.getElementById('alphaItems').appendChild(li)
-      }
-    },
-    // generateCards(obj) {
-    //   const mainLink = document.createElement('a')
-    //   mainLink.href = '#'
-    //   mainLink.style.textDecoration = 'none'
-    //   mainLink.style.color = 'black'
-    //   const mainCard = document.createElement('div')
-    //   mainCard.classList.add('card')
-    //   const cardHorizontal = document.createElement('div')
-    //   cardHorizontal.classList.add('card-horizontal')
-    //   const cardImg = document.createElement('div')
-    //   cardImg.classList.add('img-square-wrapper')
-    //   const imgRestaurant = document.createElement('img')
-    //   imgRestaurant.src = obj.featured_image
-    //   imgRestaurant.width = '300'
-    //   imgRestaurant.height = '180'
-    //   cardImg.appendChild(imgRestaurant)
-    //   const cardBody = document.createElement('div')
-    //   cardBody.classList.add('card-body')
-    //   const restaurantTitle = document.createElement('h4')
-    //   restaurantTitle.classList.add('card-title')
-    //   restaurantTitle.appendChild(document.createTextNode(obj.name))
-    //   const descriptionString = `
-    //   Address: ${obj.location.adress}
-    //   Cuisines: ${obj.cuisines}
-    //   Timings: ${obj.timings}
-    //   `
-    //   const restaurantDescription = document.createElement('p')
-    //   restaurantDescription.classList.add('card-text')
-    //   restaurantDescription.appendChild(
-    //     document.createTextNode(descriptionString)
-    //   )
-    //   cardBody.appendChild(restaurantTitle)
-    //   cardBody.appendChild(restaurantDescription)
-    //   cardHorizontal.appendChild(cardImg)
-    //   cardHorizontal.appendChild(cardBody)
-    //   mainCard.appendChild(cardHorizontal)
-    //   mainLink.appendChild(mainCard)
-    //   document.getElementById('restaurants').appendChild(mainLink)
-
-    //   // document.getElementById('restaurants').innerHTML += `
-    //   // <div class="card">
-    //   //       <div class="card-horizontal">
-    //   //         <div class="img-square-wrapper">
-    //   //           <img
-    //   //             class=""
-    //   //             src="${obj.featured_image}"
-    //   //             alt="Card image cap"
-    //   //             width= 300
-    //   //             height= 180
-    //   //           />
-    //   //         </div>
-    //   //         <div class="card-body">
-    //   //           <h4 class="card-title">${obj.name}</h4>
-    //   //           <p class="card-text">
-    //   //             Address: ${obj.location.address}
-    //   //             Cuisines: ${obj.cuisines}
-    //   //             Timings: ${obj.timings}
-    //   //           </p>
-    //   //         </div>
-    //   //       </div>
-    //   //     </div>
-    //   // `
-    // },
-    getCuisineTypes() {
-      restaurantService.getCuisines().then((res) => {
-        let i = 0
-        const cuisineTypes = res.cuisines
-        // console.log(cuisineTypes)
-        for (i; i < 8; i++) {
-          const checkboxCuisine = document.createElement('input')
-          checkboxCuisine.type = 'checkbox'
-          checkboxCuisine.value = cuisineTypes[i].cuisine.cuisine_name
-
-          const spanText = document.createElement('span')
-          spanText.appendChild(
-            document.createTextNode(' ' + cuisineTypes[i].cuisine.cuisine_name)
-          )
-
-          document.getElementById('cuisineTypes').appendChild(checkboxCuisine)
-          document.getElementById('cuisineTypes').appendChild(spanText)
-          document
-            .getElementById('cuisineTypes')
-            .appendChild(document.createElement('BR'))
-        }
-        const dotSpan = document.createElement('span')
-        dotSpan.id = 'dots'
-        dotSpan.appendChild(document.createTextNode('...'))
-        const seeMoreSpan = document.createElement('div')
-        seeMoreSpan.id = 'more'
-        seeMoreSpan.style.display = 'none'
-        const seeMoreButton = document.createElement('button')
-        seeMoreButton.onclick = 'this.seeMore'
-        seeMoreButton.type = 'button'
-        seeMoreButton.classList.add('button')
-        seeMoreButton.width = '77.48'
-        seeMoreButton.height = '33.33'
-
-        document.getElementById('cuisineTypes').appendChild(dotSpan)
-        document
-          .getElementById('cuisineTypes')
-          .appendChild(document.createElement('BR'))
-        document.getElementById('cuisineTypes').appendChild(seeMoreSpan)
-        document
-          .getElementById('cuisineTypes')
-          .appendChild(document.createElement('BR'))
-        console.log(seeMoreButton)
-        document.getElementById('cuisineTypes').appendChild(seeMoreButton)
-
-        for (i; i < cuisineTypes.length; i++) {
-          const checkboxCuisine = document.createElement('input')
-          checkboxCuisine.type = 'checkbox'
-          checkboxCuisine.value = cuisineTypes[i].cuisine.cuisine_name
-
-          const spanText = document.createElement('span')
-          spanText.appendChild(
-            document.createTextNode(' ' + cuisineTypes[i].cuisine.cuisine_name)
-          )
-          document.getElementById('more').appendChild(checkboxCuisine)
-          document.getElementById('more').appendChild(spanText)
-          document
-            .getElementById('more')
-            .appendChild(document.createElement('BR'))
-        }
-      })
-    },
-    seeMore() {
-      const dots = document.getElementById('dots')
-      const moreText = document.getElementById('more')
-      const btnText = document.getElementById('myBtn')
-
-      if (dots.style.display === 'none') {
-        dots.style.display = 'inline'
-        btnText.innerHTML = 'Read more'
-        moreText.style.display = 'none'
-      } else {
-        dots.style.display = 'none'
-        btnText.innerHTML = 'Read less'
-        moreText.style.display = 'inline'
       }
     },
   },
