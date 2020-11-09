90d54d1f3feab07218e3eca1f5f0fd30beb1cb0f
jasxmine <54893485+jasxmine@users.noreply.github.com>
2020-11-09 17:45:33 +0800

    Update queue

--- components/Counter.vue
+++ components/Counter.vue
@@ -1,36 +1,61 @@
 <template>
   <div class="container">
     <div>
-      <b-alert show variant="info"
-        ><p class="text-center">
+      <b-alert show variant="info">
+        <p class="text-center">
           Due to safe distancing, please note that the number of customers will
           not cross the limit which is currently set at <strong>50</strong>!
-        </p></b-alert
-      >
+        </p>
+      </b-alert>
     </div>
 
     <div id="app" class="justify-content-center">
-      <!-- Increase Counter, with an `increaseLimit` of 50 -->
-      <b-button variant="success" size="lg" @click="increaseCounter(50)"
-        >Increase</b-button
-      >
-      <!-- Decrease Counter, with an `decreaseLimit` of 0 -->
-      <b-button variant="warning" size="lg" @click="decreaseCounter(0)"
-        >Decrease</b-button
-      >
+      <b-row>
+        <b-col cols="10">
+          <p style="color: #f0ad4e">Currently serving <br /></p>
+          <strong>
+            <p style="color: #0b426d">Token Number <br /></p>
+          </strong>
+          <div class="div1">
+            <strong>
+              <p style="color: #f0ad4e">{{ counter }}</p>
+            </strong>
+          </div>
 
-      <b-button variant="danger" size="lg" @click="resetCounter"
-        >Reset</b-button
-      >
+          <br /><br />
+          <p>
+            <strong>
+              Waiting time <br />
+              {{ output }} minutes </strong
+            ><br />
+          </p>
+        </b-col>
+
+        <b-col cols="2">
+          <br />
+          <b-row>
+            <b-button class="btn1" size="lg" @click="increaseCounter(50)"
+              >Increase</b-button
+            >
+          </b-row>
+          <b-row>
+            <b-button class="btn2" size="lg" @click="decreaseCounter(0)"
+              >Decrease</b-button
+            >
+          </b-row>
+          <b-row>
+            <b-button class="btn3" size="lg" @click="resetCounter"
+              >Reset</b-button
+            >
+          </b-row>
+        </b-col>
+      </b-row>
 
       <br /><br /><br />
-      <p class="text-center">
-        <strong> Counter: {{ counter }}</strong> <br />
-        <strong>Result: {{ output }} </strong><br />
-      </p>
     </div>
   </div>
 </template>
+
 <script>
 export default {
   data() {
@@ -41,7 +66,7 @@ export default {
   computed: {
     output() {
       // Output for computed property!
-      return this.counter >= 50 ? 'Reached limit' : 'Less than limit'
+      return 0.5 * this.counter
     },
   },
   methods: {
@@ -69,12 +94,54 @@ p {
 button {
   margin-bottom: 1rem;
   font-size: 15px;
+  width: 100px;
 }
+
 .container {
   position: absolute;
   text-align: center;
   justify-content: center;
   width: auto;
   display: block;
+}
+
+.btn1 {
+  color: #fff !important;
+  background-color: #449474 !important;
+  border-color: #449474 !important;
+  margin-bottom: 10%;
+  margin-top: 10%;
+}
+
+.btn2 {
+  color: #fff !important;
+  background-color: #ecac8b !important;
+  border-color: #ecac8b !important;
+  margin-bottom: 5%;
+  margin-top: 5%;
+}
+
+.btn3 {
+  color: #fff !important;
+  background-color: #d5747c !important;
+  border-color: #d5747c !important;
+  margin-bottom: 10%;
+  margin-top: 10%;
+}
+
+.counter {
+  width: 50px;
+  border-color: #d5747c;
+  border: 1px;
+}
+
+.div1 {
+  display: inline-block;
+  border: 3px;
+  border-style: solid;
+  border-color: #f0ad4e;
+  padding: 1em;
+  width: 10%;
+  border-radius: 20%;
 }
 </style>
