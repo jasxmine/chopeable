58c729447889e7a4deaf2f156b88fe9eaf691c17
Rdice14 <56544853+Rdice14@users.noreply.github.com>
2020-11-13 01:02:15 +0800

    Added stylings for card components

--- components/search/cardComponent.vue
+++ components/search/cardComponent.vue
@@ -1,28 +1,48 @@
 <template>
   <a
     :href="obj.url.slice(33, obj.url.indexOf('?'))"
-    style="text-decoration: none; color: black"
+    style="text-decoration: none; color: black; margin-bottom: 5px"
   >
-    <div class="card">
+    <div
+      class="card"
+      style="
+        box-shadow: 0px 1.2px 1.2px 1.2px #888888;
+        padding: 2px 0px 2px 18px;
+        border-radius: 0%;
+      "
+    >
       <div class="card-horizontal">
-        <div class="img-square-wrapper">
-          <img
-            class=""
-            :src="obj.featured_image"
-            alt="Card image cap"
-            width="300"
-            height="180"
-          />
-        </div>
-        <div class="card-body">
-          <h4 class="card-title">{{ obj.name }}</h4>
-          <p class="card-text">
-            Address: {{ obj.location.address }}
-            <br />
-            Cuisine Type: {{ obj.cuisines }}
-            <br />
-            Opening Times: {{ obj.timings }}
-          </p>
+        <div class="row">
+          <div class="img-square-wrapper">
+            <img
+              class=""
+              :src="obj.featured_image"
+              alt="Card image cap"
+              width="300"
+              height="180"
+            />
+          </div>
+          <div class="card-body">
+            <h4 class="card-title">
+              <strong> {{ obj.name }} </strong>
+            </h4>
+            <p
+              class="card-text text-justify"
+              style="width: 700px; overflow-wrap: break-word"
+            >
+              <span style="overflow-wrap: break-word">
+                <strong> Address: </strong> {{ obj.location.address }}
+              </span>
+              <br />
+              <span style="overflow-wrap: break-word">
+                <strong> Cuisine Type: </strong> {{ obj.cuisines }}
+              </span>
+              <br />
+              <span style="overflow-wrap: break-word">
+                <strong> Opening Times: </strong> {{ obj.timings }}
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
