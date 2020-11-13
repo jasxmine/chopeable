bd36bad9bb520a0fd47e272e1038fbf2103e23f2
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-11-13 12:33:48 +0800

    Add timepicker and datepicker to reservation form

--- pages/_restaurant/reservation/index.vue
+++ pages/_restaurant/reservation/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page">
-    <h3 class="text-center my-5"><b>Complete your reservation</b></h3>
+    <h3 class="text-center m-0 py-5"><b>Complete your reservation</b></h3>
     <div class="container content">
       <h2 class="mt-1 mb-4">
         {{ restaurantData.name }}
@@ -8,40 +8,30 @@
       <div>
         <b-form v-if="show" @submit="onSubmit">
           <b-form-group
-            id="input-group-1"
-            label="Email address:"
-            label-for="input-1"
-            description="We'll never share your email with anyone else."
+            id="noOfPax-group"
+            label="Number of people:"
+            label-for="noOfPax"
           >
-            <b-form-input
-              id="input-1"
-              v-model="form.email"
-              type="email"
-              required
-              placeholder="Enter email"
-            ></b-form-input>
-          </b-form-group>
-
-          <b-form-group
-            id="input-group-2"
-            label="Your Name:"
-            label-for="input-2"
-          >
-            <b-form-input
-              id="input-2"
-              v-model="form.name"
-              required
-              placeholder="Enter name"
-            ></b-form-input>
-          </b-form-group>
-
-          <b-form-group id="input-group-3" label="Food:" label-for="input-3">
             <b-form-select
-              id="input-3"
-              v-model="form.food"
-              :options="foods"
-              required
+              id="noOfPax"
+              v-model="form.pax"
+              :options="paxOptions"
             ></b-form-select>
+          </b-form-group>
+
+          <b-form-group id="date-group-2" label="Date:" label-for="date">
+            <b-form-datepicker
+              id="date"
+              v-model="form.date"
+              class="mb-2"
+            ></b-form-datepicker>
+          </b-form-group>
+
+          <b-form-group id="input-group-3" label="Time:" label-for="input-3">
+            <b-form-timepicker
+              v-model="form.time"
+              locale="en"
+            ></b-form-timepicker>
           </b-form-group>
 
           <b-form-group id="input-group-4">
@@ -66,10 +56,17 @@ export default {
     return {
       restaurant: '',
       restaurantData: {},
+      paxOptions: [
+        { value: '1', text: '1' },
+        { value: '2', text: '2' },
+        { value: '3', text: '3' },
+        { value: '4', text: '4' },
+        { value: '5', text: '5' },
+      ],
       form: {
-        email: '',
-        name: '',
-        food: null,
+        pax: 1,
+        date: new Date(),
+        time: new Date().getTime(),
         checked: [],
       },
       foods: [
