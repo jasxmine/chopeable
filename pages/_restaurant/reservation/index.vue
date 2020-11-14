fa84f7a5a34aca0cf8a84b183570c8ed18e4e4e9
JavierWHJ <javier.wong.2018@sis.smu.edu.sg>
2020-11-14 11:07:03 +0800

    Add styling to reservation page

--- pages/_restaurant/reservation/index.vue
+++ pages/_restaurant/reservation/index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="page" @click="emptyFocusOut" @mouseover="canScroll">
-    <h3 class="text-center m-0 py-5"><b>Complete your reservation</b></h3>
-    <div class="container content">
-      <h2 class="mt-1 mb-4">
-        {{ restaurantData.name }}
-      </h2>
-      <div>
-        <b-form v-if="show" @submit="onSubmit">
+  <div class="page mb-5" @click="emptyFocusOut" @mouseover="canScroll">
+    <div class="container">
+      <b-form v-if="show" @submit="onSubmit">
+        <h3 class="text-center m-0 py-5"><b>Complete your reservation</b></h3>
+        <div class="container content">
+          <h2 class="mt-1 mb-4">
+            {{ restaurantData.name }}
+          </h2>
           <b-form-group
             id="noOfPax-group"
             label="Number of people:"
@@ -27,16 +27,61 @@
             ></b-form-datepicker>
           </b-form-group>
 
-          <b-form-group id="input-group-3" label="Time:" label-for="input-3">
+          <b-form-group id="time-group" label="Time:" label-for="time">
             <b-form-timepicker
+              id="time"
               v-model="form.time"
               locale="en"
             ></b-form-timepicker>
           </b-form-group>
+        </div>
+        <h4 class="text-center m-0 py-5"><b>Continue with</b></h4>
+        <div class="container content mt-5">
+          <b-form-group
+            id="email-group"
+            label="Email address:"
+            label-for="email"
+            description="We'll never share your email with anyone else."
+          >
+            <b-form-input
+              id="email"
+              v-model="form.email"
+              type="email"
+              required
+              placeholder="Enter email"
+            ></b-form-input>
+          </b-form-group>
 
-          <b-button type="submit" variant="warning">Reserve Now</b-button>
-        </b-form>
-      </div>
+          <b-form-group id="name-group" label="Your Name:" label-for="name">
+            <b-form-input
+              id="name"
+              v-model="form.name"
+              required
+              placeholder="Enter name"
+            ></b-form-input>
+          </b-form-group>
+        </div>
+        <h4 class="text-center m-0 py-5"><b>Additional Information</b></h4>
+        <div class="container content mt-5">
+          <b-form-group
+            id="request-group"
+            label="Special Request:"
+            label-for="request"
+            description="E.g. table preference, space for a stroller..."
+          >
+            <b-form-input
+              id="request"
+              v-model="form.request"
+              type="text"
+              required
+              placeholder="Special Request?"
+            ></b-form-input>
+          </b-form-group>
+        </div>
+        <b-button type="submit" variant="warning" block class="mt-5"
+          >Reserve Now</b-button
+        >
+      </b-form>
     </div>
   </div>
 </template>
@@ -59,6 +104,9 @@ export default {
         pax: 1,
         date: new Date(),
         time: new Date().getTime(),
+        email: '',
+        name: '',
+        request: '',
       },
       show: true,
     }
