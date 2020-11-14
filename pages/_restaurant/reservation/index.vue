ca5a575e7f78a4548c2bbb0be184e593059ba06d
wellsonah2019 <wellsonah.2019@smu.edu.sg>
2020-11-14 12:57:57 +0800

    fixed footer positioning

--- pages/_restaurant/reservation/index.vue
+++ pages/_restaurant/reservation/index.vue
@@ -1,94 +1,98 @@
 <template>
-  <div class="page" @click="emptyFocusOut" @mouseover="canScroll">
-    <div v-if="restaurantData.name" class="container">
-      <b-form v-if="show" @submit="onSubmit">
-        <h3 class="text-center m-0 py-5"><b>Complete your reservation</b></h3>
-        <div class="container content">
-          <h2 class="mt-1 mb-4">
-            {{ restaurantData.name }}
-          </h2>
-          <b-form-group
-            id="noOfPax-group"
-            label="Number of people:"
-            label-for="noOfPax"
-          >
-            <b-form-select
-              id="noOfPax"
-              v-model="form.pax"
-              :options="paxOptions"
-            ></b-form-select>
-          </b-form-group>
+  <div>
+    <div class="page" @click="emptyFocusOut" @mouseover="canScroll">
+      <div v-if="restaurantData.name" class="container">
+        <b-form v-if="show" @submit="onSubmit">
+          <h3 class="text-center m-0 py-5"><b>Complete your reservation</b></h3>
+          <div class="container content">
+            <h2 class="mt-1 mb-4">
+              {{ restaurantData.name }}
+            </h2>
+            <b-form-group
+              id="noOfPax-group"
+              label="Number of people:"
+              label-for="noOfPax"
+            >
+              <b-form-select
+                id="noOfPax"
+                v-model="form.pax"
+                :options="paxOptions"
+              ></b-form-select>
+            </b-form-group>
 
-          <b-form-group id="date-group-2" label="Date:" label-for="date">
-            <b-form-datepicker
-              id="date"
-              v-model="form.date"
-              class="mb-2"
-            ></b-form-datepicker>
-          </b-form-group>
+            <b-form-group id="date-group-2" label="Date:" label-for="date">
+              <b-form-datepicker
+                id="date"
+                v-model="form.date"
+                class="mb-2"
+              ></b-form-datepicker>
+            </b-form-group>
 
-          <b-form-group id="time-group" label="Time:" label-for="time">
-            <b-form-timepicker
-              id="time"
-              v-model="form.time"
-              locale="en"
-            ></b-form-timepicker>
-          </b-form-group>
-        </div>
-        <h4 class="text-center m-0 py-5"><b>Continue with</b></h4>
-        <div class="container content mt-5">
-          <b-form-group
-            id="email-group"
-            label="Email address:"
-            label-for="email"
-            description="We'll never share your email with anyone else."
-          >
-            <b-form-input
-              id="email"
-              v-model="form.email"
-              type="email"
-              required
-              placeholder="Enter email"
-            ></b-form-input>
-          </b-form-group>
+            <b-form-group id="time-group" label="Time:" label-for="time">
+              <b-form-timepicker
+                id="time"
+                v-model="form.time"
+                locale="en"
+              ></b-form-timepicker>
+            </b-form-group>
+          </div>
+          <h4 class="text-center m-0 py-5"><b>Continue with</b></h4>
+          <div class="container content mt-5">
+            <b-form-group
+              id="email-group"
+              label="Email address:"
+              label-for="email"
+              description="We'll never share your email with anyone else."
+            >
+              <b-form-input
+                id="email"
+                v-model="form.email"
+                type="email"
+                required
+                placeholder="Enter email"
+              ></b-form-input>
+            </b-form-group>
 
-          <b-form-group id="name-group" label="Your Name:" label-for="name">
-            <b-form-input
-              id="name"
-              v-model="form.name"
-              required
-              placeholder="Enter name"
-            ></b-form-input>
-          </b-form-group>
-        </div>
-        <h4 class="text-center m-0 py-5"><b>Additional Information</b></h4>
-        <div class="container content mt-5">
-          <b-form-group
-            id="request-group"
-            label="Special Request:"
-            label-for="request"
-            description="E.g. table preference, space for a stroller..."
+            <b-form-group id="name-group" label="Your Name:" label-for="name">
+              <b-form-input
+                id="name"
+                v-model="form.name"
+                required
+                placeholder="Enter name"
+              ></b-form-input>
+            </b-form-group>
+          </div>
+          <h4 class="text-center m-0 py-5"><b>Additional Information</b></h4>
+          <div class="container content mt-5">
+            <b-form-group
+              id="request-group"
+              label="Special Request:"
+              label-for="request"
+              description="E.g. table preference, space for a stroller..."
+            >
+              <b-form-input
+                id="request"
+                v-model="form.request"
+                type="text"
+                placeholder="Special Request?"
+              ></b-form-input>
+            </b-form-group>
+          </div>
+          <b-button type="submit" variant="warning" block class="mt-5"
+            >Reserve Now</b-button
           >
-            <b-form-input
-              id="request"
-              v-model="form.request"
-              type="text"
-              placeholder="Special Request?"
-            ></b-form-input>
-          </b-form-group>
-        </div>
-        <b-button type="submit" variant="warning" block class="mt-5"
-          >Reserve Now</b-button
-        >
-      </b-form>
-    </div>
-    <div v-else>
-      <div class="d-flex justify-content-center mt-5">
-        <b-spinner
-          style="width: 5rem; height: 5rem"
-          label="Large Spinner"
-        ></b-spinner>
+        </b-form>
       </div>
+
+      <div v-else>
+        <div class="d-flex justify-content-center mt-5">
+          <b-spinner
+            style="width: 5rem; height: 5rem"
+            label="Large Spinner"
+          ></b-spinner>
+        </div>
+      </div>
+      <theFooter />
     </div>
   </div>
 </template>
@@ -96,8 +100,13 @@
 <script>
 import emailjs from 'emailjs-com'
 import restaurantService from '../../../services/restaurantService'
+import theFooter from '../../../components/theFooter'
 
 export default {
+  components: {
+    theFooter,
+  },
+  layout: 'restaurantPage',
   data() {
     return {
       restaurant: '',
